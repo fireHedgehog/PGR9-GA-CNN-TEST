366f1391cf647b4a4870008b031b7dc5599afe28
rockychen <rj.chen91@gmail.com>
2020-10-18 20:30:20 +1300

    first commit

--- 1-digit-recognition-example/1-benchmark/Benchmark.py
+++ 1-digit-recognition-example/1-benchmark/Benchmark.py
@@ -1,3 +1,11 @@
-num_1 = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+from mnist import MNIST
+from numpy import random
+
+mndata = MNIST('samples')
+images, labels = mndata.load_training()
+
+index = random.randint(len(images))  # choose an index ;-)
+
+img = images[index]
+
+print(mndata.display(images[index]))
