2c53640424b1b98110aeca15996c7aa79ce3a199
rockychen <rj.chen91@gmail.com>
2020-10-18 20:44:03 +1300

    first commit

--- 1-digit-recognition-example/1-benchmark/Benchmark.py
+++ 1-digit-recognition-example/1-benchmark/Benchmark.py
@@ -1,11 +1,27 @@
-from mnist import MNIST
-from numpy import random
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
 
-mndata = MNIST('samples')
-images, labels = mndata.load_training()
+# MNIST_data指的是存放数据的文件夹路径，one_hot=True 为采用one_hot的编码方式编码标签
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-index = random.randint(len(images))  # choose an index ;-)
+# load data
+train_X = mnist.train.images  # 训练集样本
+validation_X = mnist.validation.images  # 验证集样本
+test_X = mnist.test.images  # 测试集样本
+# labels
+train_Y = mnist.train.labels  # 训练集标签
+validation_Y = mnist.validation.labels  # 验证集标签
+test_Y = mnist.test.labels  # 测试集标签
 
-img = images[index]
+print(train_X.shape, train_Y.shape)  # 输出训练集样本和标签的大小
 
-print(mndata.display(images[index]))
+# 查看数据，例如训练集中第一个样本的内容和标签
+print(train_X[0])  # 是一个包含784个元素且值在[0,1]之间的向量
+print(train_Y[0])
+
+# 可视化样本
+img = train_X[0].reshape(28, 28)
+print(img.shape)
+
+plt.imshow(img)
+plt.show()
