b79c439d96a0cb595bb940401d97d08c822eb842
fireHedgehog <rj.chen91@gmail.com>
2021-01-03 11:27:38 +1300

    MNIST_data / fashion mnist data

--- 2-pytorch-example/mnist_test.py
+++ 2-pytorch-example/mnist_test.py
@@ -3,363 +3,160 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
 
-print("PyTorch Version: ", torch.__version__)
+if __name__ == '__main__':
 
+    print("PyTorch Version: ", torch.__version__)
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)  # 28 * 28 -> (28+1-5) 24 * 24
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)  # 20 * 20
-        self.fc1 = nn.Linear(4 * 4 * 50, 500)
-        self.fc2 = nn.Linear(500, 10)
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(1, 20, 5, 1)  # 28 * 28 -> (28+1-5) 24 * 24
+            self.conv2 = nn.Conv2d(20, 50, 5, 1)  # 20 * 20
+            self.fc1 = nn.Linear(4 * 4 * 50, 500)
+            self.fc2 = nn.Linear(500, 10)
 
-    def forward(self, x):
-        # x: 1 * 28 * 28
-        x = F.relu(self.conv1(x))  # 20 * 24 * 24
-        x = F.max_pool2d(x, 2, 2)  # 12 * 12
-        x = F.relu(self.conv2(x))  # 8 * 8
-        x = F.max_pool2d(x, 2, 2)  # 4 *4
-        x = x.view(-1, 4 * 4 * 50)  # reshape (5 * 2 * 10), view(5, 20) -> (5 * 20)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        # return x
-        return F.log_softmax(x, dim=1)  # log probability
-
-
-# %%
-
-mnist_data = datasets.MNIST("./mnist_data", train=True, download=True,
-                            transform=transforms.Compose([
-                                transforms.ToTensor(),
-                            ]))
-mnist_data
-
-# %%
-
-data = [d[0].data.cpu().numpy() for d in mnist_data]
-
-# %%
-
-np.mean(data)
-
-# %%
-
-np.std(data)
-
-# %%
-
-mnist_data[223][0].shape
+        def forward(self, x):
+            # x: 1 * 28 * 28
+            x = F.relu(self.conv1(x))  # 20 * 24 * 24
+            x = F.max_pool2d(x, 2, 2)  # 12 * 12
+            x = F.relu(self.conv2(x))  # 8 * 8
+            x = F.max_pool2d(x, 2, 2)  # 4 *4
+            x = x.view(-1, 4 * 4 * 50)  # reshape (5 * 2 * 10), view(5, 20) -> (5 * 20)
+            x = F.relu(self.fc1(x))
+            x = self.fc2(x)
+            # return x
+            return F.log_softmax(x, dim=1)  # log probability
 
 
-# %%
+    mnist_data = datasets.MNIST("./mnist_data", train=True, download=True,
+                                transform=transforms.Compose([
+                                    transforms.ToTensor(),
+                                ]))
 
-def train(model, device, train_loader, optimizer, epoch):
-    model.train()
-    for idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+    data = [d[0].data.cpu().numpy() for d in mnist_data]
 
-        pred = model(data)  # batch_size * 10
-        loss = F.nll_loss(pred, target)
+    np.mean(data)
 
-        # SGD
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if idx % 100 == 0:
-            print("Train Epoch: {}, iteration: {}, Loss: {}".format(
-                epoch, idx, loss.item()))
+    np.std(data)
 
 
-# %%
-
-def test(model, device, test_loader):
-    model.eval()
-    total_loss = 0.
-    correct = 0.
-    with torch.no_grad():
-        for idx, (data, target) in enumerate(test_loader):
+    def train(model, device, train_loader, optimizer, epoch):
+        model.train()
+        for idx, (data, target) in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
 
-            output = model(data)  # batch_size * 10
-            total_loss += F.nll_loss(output, target, reduction="sum").item()
-            pred = output.argmax(dim=1)  # batch_size * 1
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    total_loss /= len(test_loader.dataset)
-    acc = correct / len(test_loader.dataset) * 100.
-    print("Test loss: {}, Accuracy: {}".format(total_loss, acc))
-
-
-# %%
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-batch_size = 32
-train_dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST("./mnist_data", train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=batch_size, shuffle=True,
-    num_workers=1, pin_memory=True
-)
-test_dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST("./mnist_data", train=False, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=batch_size, shuffle=True,
-    num_workers=1, pin_memory=True
-)
-
-lr = 0.01
-momentum = 0.5
-model = Net().to(device)
-optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
-
-num_epochs = 2
-for epoch in range(num_epochs):
-    train(model, device, train_dataloader, optimizer, epoch)
-    test(model, device, test_dataloader)
-
-torch.save(model.state_dict(), "mnist_cnn.pt")
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-batch_size = 32
-train_dataloader = torch.utils.data.DataLoader(
-    datasets.FashionMNIST("./fashion_mnist_data", train=True, download=True,
-                          transform=transforms.Compose([
-                              transforms.ToTensor(),
-                              transforms.Normalize((0.1307,), (0.3081,))
-                          ])),
-    batch_size=batch_size, shuffle=True,
-    num_workers=1, pin_memory=True
-)
-test_dataloader = torch.utils.data.DataLoader(
-    datasets.FashionMNIST("./fashion_mnist_data", train=False, download=True,
-                          transform=transforms.Compose([
-                              transforms.ToTensor(),
-                              transforms.Normalize((0.1307,), (0.3081,))
-                          ])),
-    batch_size=batch_size, shuffle=True,
-    num_workers=1, pin_memory=True
-)
-
-lr = 0.01
-momentum = 0.5
-model = Net().to(device)
-optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
-
-num_epochs = 2
-for epoch in range(num_epochs):
-    train(model, device, train_dataloader, optimizer, epoch)
-    test(model, device, test_dataloader)
-
-torch.save(model.state_dict(), "fashion_mnist_cnn.pt")
-
-import numpy as np
-import torchvision
-from torchvision import datasets, transforms, models
-
-import matplotlib.pyplot as plt
-import os
-import copy
-
-print("Torchvision Version: ", torchvision.__version__)
-
-# Top level data directory. Here we assume the format of the directory conforms
-#   to the ImageFolder structure
-data_dir = "./hymenoptera_data"
-# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
-model_name = "resnet"
-# Number of classes in the dataset
-num_classes = 2
-# Batch size for training (change depending on how much memory you have)
-batch_size = 32
-# Number of epochs to train for
-num_epochs = 15
-# Flag for feature extracting. When False, we finetune the whole model,
-#   when True we only update the reshaped layer params
-feature_extract = True
-
-input_size = 224
-
-all_imgs = datasets.ImageFolder(os.path.join(data_dir, "train"), transforms.Compose([
-    transforms.RandomResizedCrop(input_size),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-]))
-loader = torch.utils.data.DataLoader(all_imgs, batch_size=batch_size, shuffle=True, num_workers=4)
-
-# %%
-
-data_transforms = {
-    "train": transforms.Compose([
-        transforms.RandomResizedCrop(input_size),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    "val": transforms.Compose([
-        transforms.Resize(input_size),
-        transforms.CenterCrop(input_size),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-}
-
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ["train", "val"]}
-
-dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x],
-                                                   batch_size=batch_size, shuffle=True, num_workers=4) for x in
-                    ["train", "val"]}
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# %%
-
-img = next(iter(dataloaders_dict["val"]))[0]
-
-# %%
-
-img.shape
-
-# %%
-
-unloader = transforms.ToPILImage()  # reconvert into PIL image
-
-plt.ion()
-
-
-def imshow(tensor, title=None):
-    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
-    image = image.squeeze(0)  # remove the fake batch dimension
-    image = unloader(image)
-    plt.imshow(image)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
-
-
-plt.figure()
-imshow(img[11], title='Image')
-
-
-# %%
-
-def set_parameter_requires_grad(model, feature_extract):
-    if feature_extract:
-        for param in model.parameters():
-            param.requires_grad = False
-
-
-def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
-    if model_name == "resnet":
-        model_ft = models.resnet18(pretrained=use_pretrained)
-        set_parameter_requires_grad(model_ft, feature_extract)
-        num_ftrs = model_ft.fc.in_features
-        model_ft.fc = nn.Linear(num_ftrs, num_classes)
-        input_size = 224
-    else:
-        print("model not implemented")
-        return None, None
-
-    return model_ft, input_size
-
-
-model_ft, input_size = initialize_model(model_name,
-                                        num_classes, feature_extract, use_pretrained=True)
-print(model_ft)
-
-# %%
-
-model_ft.layer1[0].conv1.weight.requires_grad
-
-# %%
-
-model_ft.fc.weight.requires_grad
-
-
-# %%
-
-def train_model(model, dataloaders, loss_fn, optimizer, num_epochs=5):
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.
-    val_acc_history = []
-    for epoch in range(num_epochs):
-        for phase in ["train", "val"]:
-            running_loss = 0.
-            running_corrects = 0.
-            if phase == "train":
-                model.train()
-            else:
-                model.eval()
-
-            for inputs, labels in dataloaders[phase]:
-                inputs, labels = inputs.to(device), labels.to(device)
-
-                with torch.autograd.set_grad_enabled(phase == "train"):
-                    outputs = model(inputs)  # bsize * 2
-                    loss = loss_fn(outputs, labels)
-
-                preds = outputs.argmax(dim=1)
-                if phase == "train":
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds.view(-1) == labels.view(-1)).item()
-
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = running_corrects / len(dataloaders[phase].dataset)
-
-            print("Phase {} loss: {}, acc: {}".format(phase, epoch_loss, epoch_acc))
-
-            if phase == "val" and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-            if phase == "val":
-                val_acc_history.append(epoch_acc)
-    model.load_state_dict(best_model_wts)
-    return model, val_acc_history
-
-
-model_ft = model_ft.to(device)
-
-# %%
-
-optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad,
-                                   model_ft.parameters()), lr=0.001, momentum=0.9)
-loss_fn = nn.CrossEntropyLoss()
-
-# %%
-
-_, ohist = train_model(model_ft, dataloaders_dict, loss_fn, optimizer, num_epochs=num_epochs)
-
-# %%
-
-model_scratch, _ = initialize_model(model_name,
-                                    num_classes, feature_extract=False, use_pretrained=False)
-model_scratch = model_scratch.to(device)
-optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad,
-                                   model_scratch.parameters()), lr=0.001, momentum=0.9)
-loss_fn = nn.CrossEntropyLoss()
-_, scratch_hist = train_model(model_scratch, dataloaders_dict, loss_fn, optimizer, num_epochs=num_epochs)
-
-# Plot the training curves of validation accuracy vs. number
-#  of training epochs for the transfer learning method and
-#  the model trained from scratch
-
-plt.title("Validation Accuracy vs. Number of Training Epochs")
-plt.xlabel("Training Epochs")
-plt.ylabel("Validation Accuracy")
-plt.plot(range(1, num_epochs + 1), ohist, label="Pretrained")
-plt.plot(range(1, num_epochs + 1), scratch_hist, label="Scratch")
-plt.ylim((0, 1.))
-plt.xticks(np.arange(1, num_epochs + 1, 1.0))
-plt.legend()
-plt.show()
+            pred = model(data)  # batch_size * 10
+            loss = F.nll_loss(pred, target)
+            # SGD
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if idx % 100 == 0:
+                print("Train Epoch: {}, iteration: {}, Loss: {}".format(
+                    epoch, idx, loss.item()))
+
+
+    def test(model, device, test_loader):
+        model.eval()
+        total_loss = 0.
+        correct = 0.
+        with torch.no_grad():
+            for idx, (data, target) in enumerate(test_loader):
+                data, target = data.to(device), target.to(device)
+
+                output = model(data)  # batch_size * 10
+                total_loss += F.nll_loss(output, target, reduction="sum").item()
+                pred = output.argmax(dim=1)  # batch_size * 1
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        total_loss /= len(test_loader.dataset)
+        acc = correct / len(test_loader.dataset) * 100.
+        print("Test loss: {}, Accuracy: {}".format(total_loss, acc))
+
+
+    def mnist_test():
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        batch_size = 32
+        train_dataloader = torch.utils.data.DataLoader(
+            datasets.MNIST("./mnist_data", train=True, download=True,
+                           transform=transforms.Compose([
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.1307,), (0.3081,))
+                           ])),
+            batch_size=batch_size, shuffle=True,
+            num_workers=1, pin_memory=True
+        )
+        test_dataloader = torch.utils.data.DataLoader(
+            datasets.MNIST("./mnist_data", train=False, download=True,
+                           transform=transforms.Compose([
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.1307,), (0.3081,))
+                           ])),
+            batch_size=batch_size, shuffle=True,
+            num_workers=1, pin_memory=True
+        )
+
+        lr = 0.01
+        momentum = 0.5
+        model = Net().to(device)
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+
+        num_epochs = 2
+        for epoch in range(num_epochs):
+            train(model, device, train_dataloader, optimizer, epoch)
+            test(model, device, test_dataloader)
+
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
+
+    def fashion_mnist():
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        batch_size = 32
+        train_dataloader = torch.utils.data.DataLoader(
+            datasets.FashionMNIST("./fashion_mnist_data", train=True, download=True,
+                                  transform=transforms.Compose([
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.1307,), (0.3081,))
+                                  ])),
+            batch_size=batch_size, shuffle=True,
+            num_workers=1, pin_memory=True
+        )
+        test_dataloader = torch.utils.data.DataLoader(
+            datasets.FashionMNIST("./fashion_mnist_data", train=False, download=True,
+                                  transform=transforms.Compose([
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.1307,), (0.3081,))
+                                  ])),
+            batch_size=batch_size, shuffle=True,
+            num_workers=1, pin_memory=True
+        )
+
+        lr = 0.01
+        momentum = 0.5
+        model = Net().to(device)
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+
+        num_epochs = 2
+        for epoch in range(num_epochs):
+            train(model, device, train_dataloader, optimizer, epoch)
+            test(model, device, test_dataloader)
+
+        torch.save(model.state_dict(), "fashion_mnist_cnn.pt")
+
+
+    def plot_acc(num_epochs, ohist, scratch_hist):
+        plt.title("Validation Accuracy vs. Number of Training Epochs")
+        plt.xlabel("Training Epochs")
+        plt.ylabel("Validation Accuracy")
+        plt.plot(range(1, num_epochs + 1), ohist, label="Pretrained")
+        plt.plot(range(1, num_epochs + 1), scratch_hist, label="Scratch")
+        plt.ylim((0, 1.))
+        plt.xticks(np.arange(1, num_epochs + 1, 1.0))
+        plt.legend()
+        plt.show()
+
+
+    mnist_test()
